5ce42619ef34578e32643e7cd829521f24fa7132
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-06-11 19:53:40 +0530

    vishwajeet singh assingment.PenPerStudent

--- Day-1/PenPerStudent.java
+++ Day-1/PenPerStudent.java
@@ -2,7 +2,7 @@
 public class PenPerStudent {
     public static void main(String[] args) {
         int totalPens=14,students=3;
-        int pensPerStudent=14/3, remaining=14%3;
+        int pensPerStudent=totalPens/students, remaining=totalPens%students;
 	    System.out.println("The Pen per Student is "+pensPerStudent+" and the remaining pen not distributed is "+remaining);
     }
 }
