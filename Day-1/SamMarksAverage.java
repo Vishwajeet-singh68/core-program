ead71c657143b3253f95deedd363738118f42718
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-06-11 11:44:36 +0530

    vishwajeet singh day1.SamPCMMarksAverage

---
+++ Day-1/SamMarksAverage.java
@@ -0,0 +1,9 @@
+
+public class SamMarksAverage {
+    public static void main(String[] args) {
+        int maths=94, physics=95, chemistry=96;//Storing all the marks of sam according to their subjects.
+        int sum = maths + physics + chemistry;//calculating the sum of marks scored int maths, physics and chemistry.
+        double average=sum/3.0;//As there is theree subjects the sum is devided by three.
+        System.out.println("Sam's average mark in PCM is "+average);
+    }
+}
