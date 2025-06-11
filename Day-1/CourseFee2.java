a3c2d36ebf440536439e9e624f632c16f125cbea
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-06-11 22:41:55 +0530

    vishwajeet singh new.height converter

--- Day-1/CourseFee2.java
+++ Day-1/CourseFee2.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class CourseFee2 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        System.out.println("Enter the fee:");
         double fee = in.nextDouble();
+        System.out.println("Enter the discount percentage:");
         double discountPercent = in.nextDouble();
         double discount = (fee * discountPercent) / 100;
         double finalFee = fee - discount;
